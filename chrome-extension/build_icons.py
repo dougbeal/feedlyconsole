e05083699293fadb7168d9100ff4003c9385897c
Douglas Beal <git@dougbeal.com>
2014-01-03 17:50:29 -0800

    convert .svg icon to 19 and 38

--- chrome-extension/build_icons.py
+++ chrome-extension/build_icons.py
@@ -12,14 +12,23 @@ destination = "icon"
 # Creative Common Package
 print "source " + source
 name = "utilities-terminal-5.png"
+f,e = os.path.splitext(name)
+svg = "%s.svg" % f
+
 sizes = ( 16, 22, 32, 48, 128 )
 info = {}
 for root, dirs, files in os.walk(source):
+    if svg in files:
+        source_path = os.path.join(root, svg)
+        destination_path = os.path.join(destination, svg)
+        info['svg'] = destination_path
+        shutil.copyfile(source_path,
+                        destination_path)        
     if name in files:
         path = root.split(os.sep)
         size = path[-2].split('x')[0]
         if int(size) in sizes:
-            f,e = os.path.splitext(name)
+
             newname = "%s_%s%s" % (f, size, e)
             pp(newname)
 
@@ -32,8 +41,17 @@ for root, dirs, files in os.walk(source):
             shutil.copyfile(source_path,
                             destination_path)
 
+
+sizes = [ 19, 38 ]
+source = os.path.join(destination, svg)
+for size in sizes:
+    destination_path = os.path.join(destination, "%s_%i%s" % (f, size, e))
+    info[size] = destination_path
+    os.system("convert -resample %ix%i %s %s" % (size, size, source, destination_path))
+
 f = open(os.path.join(destination, "icon_info.json"), 'w')
 json_info = json.dumps(info, sort_keys=True, indent=4)
 print json_info
 f.write(json_info)
+
 
