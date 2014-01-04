e7082d997471690051406358e4bafdb55872807e
Douglas Beal <git@dougbeal.com>
2014-01-03 17:57:17 -0800

    updated paths

--- chrome-extension/build.sh
+++ chrome-extension/build.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-cp -vf josh.js/js/*.js chrome-extension/
-( 
-    cd chrome-extension/
-    if [ ! -e jquery.min.js ]; then
-        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" 
-        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.map" 
-    fi
-    if [ ! -e jquery-ui.min.js ]; then
-        curl -qO "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"
-        curl -qO "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.map"
-    fi
-    if [ ! -e source-code-pro.css ]; then
-        curl -q "http://fonts.googleapis.com/css?family=Source+Code+Pro" > source-code-pro.css
-    fi
-    if [ ! -e underscore-min.js ]; then
-        curl -qO "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js"
-    fi
-)
+cp -vf ../josh.js/js/*.js .
+if [ ! -e jquery.min.js ]; then
+    curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" 
+    curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.map" 
+fi
+if [ ! -e jquery-ui.min.js ]; then
+    curl -qO "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"
+    curl -qO "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.map"
+fi
+if [ ! -e source-code-pro.css ]; then
+    curl -q "http://fonts.googleapis.com/css?family=Source+Code+Pro" > source-code-pro.css
+fi
+if [ ! -e underscore-min.js ]; then
+    curl -qO "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js"
+fi
+
