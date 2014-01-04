8790244b483d3b5adf075a0fc95cb7eadd517d32
Douglas Beal <git@dougbeal.com>
2014-01-03 17:06:51 -0800

    first pass at extension

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cp -vf feedlyconsole.js chrome-extension/
+cp -vf feedlyconsole.html chrome-extension/
+cp -vf manifest.json chrome-extension/
+cp -vf josh.js/js/*.js chrome-extension/
+cp -vf stylesheets/*.css chrome-extension/
+( 
+    cd chrome-extension/
+    if [ ! -e jquery.min.js ]; then
+        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" 
+        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.map" 
+    fi
+    if [ ! -e jquery-ui.min.js ]; then
+        curl -qO "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"
+        curl -qO "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.map"
+    fi
+    if [ ! -e source-code-pro.css ]; then
+        curl -q "http://fonts.googleapis.com/css?family=Source+Code+Pro" > source-code-pro.css
+    fi
+    if [ ! -e underscore-min.js ]; then
+        curl -qO "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js"
+    fi
+)
