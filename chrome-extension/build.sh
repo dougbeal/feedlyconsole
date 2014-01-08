9824e91f2229ca389f6635bf11e0a4d488bc3dc5
Douglas Beal <git@dougbeal.com>
2014-01-07 19:28:56 -0800

    added optparse factored out copying and added chmod'ing to remove writable

--- chrome-extension/build.sh
+++ chrome-extension/build.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
+function copy_chmod() {
+   for filename in ${filenames[@]}; do
+        file=${filename}.${ext}
+        cp -vf $source_dir/$file .
+
+    done
+}
 (
     cd javascript
-    cp -vf ../../josh.js/js/{readline,history,killring,pathhandler,shell}.js .
+    filenames=( "readline" "history" "killring" "pathhandler" "shell" )
+    source_dir=../../josh.js/js
+    ext="js"
+
+    copy_chmod
+
+    filenames=( "optparse" )
+    source_dir=../../josh.js/ext/optparse-js/lib
+
+    copy_chmod
+
     if [ ! -e jquery.min.js ]; then
-        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.js" 
-        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" 
-        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.map" 
+        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.js"
+        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"
+        curl -qO "http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.map"
     fi
     if [ ! -e jquery-ui.min.js ]; then
         curl -qO "http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"
@@ -13,7 +30,9 @@
     fi
     if [ ! -e underscore-min.js ]; then
         curl -qO "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js"
+
     fi
+    chmod a-w *
 )
 (
     cd stylesheets
@@ -23,5 +42,5 @@
     if [ ! -e jquery-ui.css ]; then
         curl -qO "http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css"
     fi
+    chmod a-w *
 )
-
